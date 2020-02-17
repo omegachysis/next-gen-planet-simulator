114dc9d46e1f9e91f7fcc211c24c94dedd7739b0
Matthew A. Robinson <omegachysis@outlook.com>
2020-02-17 18:40:18 -0500

    Move shaders to separate files

--- App/core/src/edu/psu/planetsim/PlanetSim.java
+++ App/core/src/edu/psu/planetsim/PlanetSim.java
@@ -1,6 +1,7 @@
 package edu.psu.planetsim;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Mesh;
@@ -19,29 +20,16 @@ public class PlanetSim extends ApplicationAdapter {
 	@Override
 	public void create () {
 //		batch = new SpriteBatch();
-		String vertexShader = "attribute vec4 a_position;    \n" + 
-                "attribute vec4 a_color;\n" +
-                "attribute vec2 a_texCoord0;\n" + 
-                "uniform mat4 u_projTrans;\n" + 
-                "varying vec4 v_color;" + 
-                "varying vec2 v_texCoords;" + 
+		String vertexShader =
+				"attribute vec4 a_position;    \n" +
                 "void main()                  \n" + 
-                "{                            \n" + 
-                "   v_color = vec4(1, 1, 1, 1); \n" + 
-                "   v_texCoords = a_texCoord0; \n" + 
-                "   gl_Position =  u_projTrans * a_position;  \n"      + 
+                "{                            \n" +
+                "   gl_Position =  a_position;  \n"      +
                 "}                            \n" ;
-		String fragmentShader = "#ifdef GL_ES\n" +
-                  "precision mediump float;\n" + 
-                  "#endif\n" + 
-                  "varying vec4 v_color;\n" + 
-                  "varying vec2 v_texCoords;\n" + 
-                  "uniform sampler2D u_texture;\n" + 
-                  "void main()                                  \n" + 
-                  "{                                            \n" + 
-                  "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n" +
-                  "}";
-		testShader = new ShaderProgram(vertexShader, fragmentShader);
+
+		testShader = new ShaderProgram(Gdx.files.getFileHandle("vert.vert", Files.FileType.Internal),
+				Gdx.files.getFileHandle("frag.frag", Files.FileType.Internal));
+
 		if (testMesh == null) {
 			testMesh = new Mesh(true, 3, 3, new VertexAttribute(Usage.Position, 3, "a_position"));		
 
