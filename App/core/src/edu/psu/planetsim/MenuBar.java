1a8f2cd33999e58d8ba66356baf8dd9fe7e75527
fbk5061 <56009387+fbk5061@users.noreply.github.com>
2020-02-22 11:31:40 -0500

    changed MenuBar

---
+++ App/core/src/edu/psu/planetsim/MenuBar.java
@@ -0,0 +1,24 @@
+package edu.psu.planetsim;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
+
+
+public class MenuBar{
+    private Stage stage;
+    private Table table;
+
+    public void create() {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        table.setDebug(true);
+    }
+}
