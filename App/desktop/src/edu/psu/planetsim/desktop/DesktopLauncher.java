8678142666865fbbfbbb5e5eb4ed95029225ea6a
Danny Ruan <ranny.duan@gmail.com>
2020-02-24 03:11:35 -0500

    Editd to include imports for PlanetRenderTest.java
    Comment out "new LwjglApplication(new PlanetSim(), config);" and uncomment "new LwjglApplication(new PlanetRenderTest(), config);" to run PlanetRenderTest.java

--- App/desktop/src/edu/psu/planetsim/desktop/DesktopLauncher.java
+++ App/desktop/src/edu/psu/planetsim/desktop/DesktopLauncher.java
@@ -2,6 +2,8 @@ package edu.psu.planetsim.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import edu.psu.planetsim.PlanetRenderTest;
 import edu.psu.planetsim.PlanetSim;
 
 public class DesktopLauncher {
@@ -16,5 +18,6 @@ public class DesktopLauncher {
 		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new PlanetSim(), config);
+		//new LwjglApplication(new PlanetRenderTest(), config);
 	}
 }
