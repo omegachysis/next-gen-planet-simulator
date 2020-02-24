bdc3ae32e411fc9ada60416ca8e91f3cb0f9739b
Danny Ruan <ranny.duan@gmail.com>
2020-02-24 02:59:00 -0500

    Test file generates multiple colored shapes

--- App/core/src/edu/psu/planetsim/PlanetRenderTest.java
+++ App/core/src/edu/psu/planetsim/PlanetRenderTest.java
@@ -13,13 +13,19 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class PlanetRenderTest implements ApplicationListener {
 	
 	public PerspectiveCamera cam;
+	public CameraInputController camController;
 	public Model model;
+	public Model model1;
+	public Model model2;
     public ModelInstance instance;
+    public ModelInstance instance1;
+    public ModelInstance instance2;
     public ModelBatch modelBatch;
     public Environment environment;
 
@@ -34,6 +40,8 @@ public class PlanetRenderTest implements ApplicationListener {
         cam.near = 1f;
         cam.far = 200f;
         cam.update();
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
         
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.2f, 0.2f, 0.2f, 1f));
@@ -44,15 +52,29 @@ public class PlanetRenderTest implements ApplicationListener {
             new Material(ColorAttribute.createDiffuse(Color.WHITE)),
             Usage.Position | Usage.Normal);
         instance = new ModelInstance(model);
+        
+        model1 = modelBuilder.createSphere(6f, 6f, 6f, 10, 10,
+                new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+                Usage.Position | Usage.Normal);
+        instance1 = new ModelInstance(model1, 6f, 6f, 0f);
+        
+        model2 = modelBuilder.createCone(4f, 3f, 4f, 10,
+                new Material(ColorAttribute.createDiffuse(Color.RED)),
+                Usage.Position | Usage.Normal);
+        instance2 = new ModelInstance(model2, 0f, 5f, 0f);
     }
     
     @Override
     public void render () {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        
+        camController.update();
 
         modelBatch.begin(cam);
         modelBatch.render(instance, environment);
+        modelBatch.render(instance1, environment);
+        modelBatch.render(instance2, environment);
         modelBatch.end();
     }
 
