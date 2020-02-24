96ef54a2c4994b86da377de2bc917df2f8bd46f1
Danny Ruan <ranny.duan@gmail.com>
2020-02-24 02:35:56 -0500

    Add files via upload
    3D rendering test file; will not display by default

---
+++ App/core/src/edu/psu/planetsim/PlanetRenderTest.java
@@ -0,0 +1,69 @@
+package edu.psu.planetsim;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+public class PlanetRenderTest implements ApplicationListener {
+	
+	public PerspectiveCamera cam;
+	public Model model;
+    public ModelInstance instance;
+    public ModelBatch modelBatch;
+
+    @Override
+    public void create () {
+    	
+    	modelBatch = new ModelBatch();
+
+        cam = new PerspectiveCamera(60, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 200f;
+        cam.update();
+        
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f, 
+            new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+            Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);
+    }
+    
+    @Override
+    public void render () {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance);
+        modelBatch.end();
+    }
+
+    @Override
+    public void dispose () {
+    	modelBatch.dispose();
+    	model.dispose();
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void pause () {
+    }
+}
