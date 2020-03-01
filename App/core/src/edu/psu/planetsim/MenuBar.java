d50f5dd9fcf30f18244f281dbd8d4b6550999d7c
Owner <owner@Owners-MacBook-Pro.local>
2020-03-01 10:36:50 -0500

    Updates on MenuBar

--- App/core/src/edu/psu/planetsim/MenuBar.java
+++ App/core/src/edu/psu/planetsim/MenuBar.java
@@ -1,111 +1,77 @@
 package edu.psu.planetsim;
 
-
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-//import com.badlogic.gdx.scenes.scene2d.ui.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.Gdx;
-
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import java.awt.*;
 
-public class MenuBar {
+public class MenuBar extends ApplicationAdapter {
 
-    public Stage mainTable;
-    public Table menuItems;
-    public Menu currentMenu;
-    public Array<Menu> menus = new Array<Menu>();
-    public MenuBarListener menuListener;
+    private Stage mainTable;
+    private Skin skin;
 
-    public TextButton button1;
-    public TextButton button2;
-    public TextButton button3;
-    public TextButton button4;
+    @Override
+    public void create() {
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        mainTable = new Stage(new ScreenViewport());
 
-    public TextButton.TextButtonStyle textButtonStyle;
-    BitmapFont font;
+        final TextButton button1 = new TextButton("File", skin, "default");
+        button1.setWidth(100);
+        button1.setHeight(20);
+        button1.setPosition(20,680);
+        mainTable.addActor(button1);
 
-    public void create () {
+        button1.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                button1.setText("File Clicked");
+            }
+        });
 
-        mainTable = new Stage();
-        Gdx.input.setInputProcessor(mainTable);
+        final TextButton button2 = new TextButton("Edit", skin, "default");
+        button2.setWidth(100);
+        button2.setHeight(20);
+        button2.setPosition(120,680);
+        button2.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                button2.setText("Edit Clicked");
+            }
+        });
 
-        menuItems = new Table();
-        menuItems.setSize(1200, 25);
-        menuItems.setPosition(16, 675);
-        mainTable.addActor(menuItems);
-        menuItems.setDebug(true);
+        final TextButton button3 = new TextButton("View", skin, "default");
+        button3.setWidth(100);
+        button3.setHeight(20);
+        button3.setPosition(220,680);
+        button3.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                button3.setText("File Clicked");
+            }
+        });
 
-        font = new BitmapFont();
-        textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.font = font;
-
-        button1 = new TextButton("File", textButtonStyle);
-        button2 = new TextButton("Edit", textButtonStyle);
-        button3 = new TextButton("View", textButtonStyle);
-        button4 = new TextButton("Tools", textButtonStyle);
         mainTable.addActor(button1);
         mainTable.addActor(button2);
         mainTable.addActor(button3);
-        mainTable.addActor(button4);
 
-
-        button1.setSize(200, 20);
-        button1.setPosition(-50, 675);
-
-        button2.setSize(200, 20);
-        button2.setPosition(25, 675);
-
-        button3.setSize(200, 20);
-        button3.setPosition(100, 675);
-
-        button4.setSize(200, 20);
-        button4.setPosition(175, 675);
-
+        Gdx.input.setInputProcessor(mainTable);
     }
 
-    private void setMenuListener(MenuBarListener menuListener) {
-        this.menuListener = menuListener;
-    }
-
-    public Stage getTable () {
-        return mainTable;
-    }
-
-    public static class MenuBarStyle {
-        public Drawable background;
-
-        public MenuBarStyle () {
-        }
-
-        public MenuBarStyle (MenuBarStyle style) {
-            this.background = style.background;
-        }
-
-        public MenuBarStyle (Drawable background) {
-            this.background = background;
-        }
-    }
-
-    public interface MenuBarListener {
-        void menuOpened (Menu menu);
-
-        void menuClosed (Menu menu);
-    }
-
-    public void render() {
+    @Override
+    public void render () {
         mainTable.act(Gdx.graphics.getDeltaTime());
         mainTable.draw();
-
     }
 
-    public void dispose() {
+    @Override
+    public void dispose () {
 
         mainTable.dispose();
     }
