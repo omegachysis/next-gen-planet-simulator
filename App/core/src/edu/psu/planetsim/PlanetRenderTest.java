d4b4a5b851a51e67e4f720c553c129bec8940df5
Danny Ruan <ranny.duan@gmail.com>
2020-03-01 21:09:44 -0500

    Planet model poles fixed
    Now poles all share one vertex coordinate

--- App/core/src/edu/psu/planetsim/PlanetRenderTest.java
+++ App/core/src/edu/psu/planetsim/PlanetRenderTest.java
@@ -59,11 +59,11 @@ public class PlanetRenderTest implements ApplicationListener {
         instance = new ModelInstance(model);
         */
         
-        int latitude = 64;
+        int latitude = 180;
         
-        int longitude = 64;
+        int longitude = 180;
         
-        model1 = modelBuilder.createSphere(1f, 1f, 1f, longitude, latitude,
+        model1 = modelBuilder.createSphere(5f, 5f, 5f, longitude, latitude,
                 new Material(ColorAttribute.createDiffuse(Color.WHITE)),
                 Usage.Position | Usage.Normal);
 
@@ -79,21 +79,13 @@ public class PlanetRenderTest implements ApplicationListener {
         mesh1.getVertices(verticesHolder);
 
         // We'll get some input map of elevations by latitude/longitude:
-        float[] elevationMap = new float[latitude * longitude + 2];
+        float[] elevationMap = new float[((latitude-1) * longitude) + 2];
         
         System.out.println(verticesHolder.length);
         
         for (int i = 0; i < verticesHolder.length / 6; i++)
         {
-        	// Vertex format goes like pppnnnpppnnn...
-            // where p are position components, n are normal components.
-            float px = verticesHolder[i * 6 + 0];
-            float py = verticesHolder[i * 6 + 1];
-            float pz = verticesHolder[i * 6 + 2];
-            float nx = verticesHolder[i * 6 + 3];
-            float ny = verticesHolder[i * 6 + 4];
-            float nz = verticesHolder[i * 6 + 5];
-                
+        	    
             // Note: this does not work on the pole vertices (e.g. for latitude = 4 and longitude = 4, verticesHolder[0]
             // actually corresponds to verticesHolder[3], not verticesHolder[4]; likewise verticesHolder[21] corresponds
             // to verticesHolder[24], not verticesHolder[20].
@@ -103,14 +95,38 @@ public class PlanetRenderTest implements ApplicationListener {
                 verticesHolder[i * 6 + 1] = verticesHolder[(i - longitude) * 6 + 1];
                 verticesHolder[i * 6 + 2] = verticesHolder[(i - longitude) * 6 + 2];
         	}
+        	else if (i <= longitude && i != 0)
+        	{
+        		verticesHolder[i * 6 + 0] = verticesHolder[0 * 6 + 0];
+                verticesHolder[i * 6 + 1] = verticesHolder[0 * 6 + 1];
+                verticesHolder[i * 6 + 2] = verticesHolder[0 * 6 + 2];
+        	}
+        	else if (i > (longitude * (latitude + 1)))
+        	{
+        		verticesHolder[i * 6 + 0] = verticesHolder[(longitude * (latitude + 1)) * 6 + 0];
+                verticesHolder[i * 6 + 1] = verticesHolder[(longitude * (latitude + 1)) * 6 + 1];
+                verticesHolder[i * 6 + 2] = verticesHolder[(longitude * (latitude + 1)) * 6 + 2];
+        	}
         	else
         	{
                 // Don't worry about the normal vectors yet... it gets pretty complicated 
                 // and it only affects lighting.
-                verticesHolder[i * 6 + 0] *= 5f + rand.nextDouble();
-                verticesHolder[i * 6 + 1] *= 5f + rand.nextDouble();
-                verticesHolder[i * 6 + 2] *= 5f + rand.nextDouble();	
+                verticesHolder[i * 6 + 0] += 0.1f * rand.nextDouble();
+                verticesHolder[i * 6 + 1] += 0.1f * rand.nextDouble();
+                verticesHolder[i * 6 + 2] += 0.1f * rand.nextDouble();	
         	}
+        	
+        	// Vertex format goes like pppnnnpppnnn...
+            // where p are position components, n are normal components.
+            float px = verticesHolder[i * 6 + 0];
+            float py = verticesHolder[i * 6 + 1];
+            float pz = verticesHolder[i * 6 + 2];
+            float nx = verticesHolder[i * 6 + 3];
+            float ny = verticesHolder[i * 6 + 4];
+            float nz = verticesHolder[i * 6 + 5];
+            
+            //elevationMap[]
+        	
         }
         mesh1.setVertices(verticesHolder);
 
