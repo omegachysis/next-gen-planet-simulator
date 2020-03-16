98ca942f84af9a07dcd61253647a3b2bd142124a
Danny Ruan <ranny.duan@gmail.com>
2020-03-15 22:37:38 -0400

    Second iteration of texturing
    Fixed bug where planet model was generating incorrectly

--- App/core/src/edu/psu/planetsim/PlanetRenderTest.java
+++ App/core/src/edu/psu/planetsim/PlanetRenderTest.java
@@ -78,7 +78,7 @@ public class PlanetRenderTest implements ApplicationListener {
 
         mesh1 = model1.meshes.first();
         
-        FileHandle imageFileHandle = Gdx.files.getFileHandle("textest1.jpg", Files.FileType.Internal);
+        FileHandle imageFileHandle = Gdx.files.getFileHandle("textest2.jpg", Files.FileType.Internal);
         texture1 = new Texture(imageFileHandle);
         TextureAttribute textureAttribute1 = new TextureAttribute(TextureAttribute.Diffuse, texture1);
 
@@ -86,17 +86,18 @@ public class PlanetRenderTest implements ApplicationListener {
         // thus every vertex has a Position vector and a Normal vector.
         // Each of these has three float components, thus the whole vertex array 
         // has 6 floats for every vertex:
-        float[] verticesHolder = new float[mesh1.getNumVertices() * 6];
+        float[] verticesHolder = new float[mesh1.getNumVertices() * 8];
         mesh1.getVertices(verticesHolder);
 
         // We'll get some input map of elevations by latitude/longitude:
         float[] elevationMap = new float[((latitude-1) * longitude) + 2];
         
-        // System.out.println(verticesHolder.length);
+        System.out.println(mesh1.getNumVertices());
+        System.out.println(verticesHolder.length);
         Vector3 planetCenter = new Vector3();
         
         int f = 0;
-        for (int i = 0; i < verticesHolder.length / 6; i++)
+        for (int i = 0; i < verticesHolder.length / 8; i++)
         {
         	    
             // Note: this does not work on the pole vertices (e.g. for latitude = 4 and longitude = 4, verticesHolder[0]
@@ -106,44 +107,46 @@ public class PlanetRenderTest implements ApplicationListener {
         	// This condition connects the last longitude with the first longitude to share the same vertex per latitude.
         	if ((i+1) % (longitude+1) == 0 && i != 0)
         	{
-        		verticesHolder[i * 6 + 0] = verticesHolder[(i - longitude) * 6 + 0];
-                verticesHolder[i * 6 + 1] = verticesHolder[(i - longitude) * 6 + 1];
-                verticesHolder[i * 6 + 2] = verticesHolder[(i - longitude) * 6 + 2];
+        		verticesHolder[i * 8 + 0] = verticesHolder[(i - longitude) * 8 + 0];
+                verticesHolder[i * 8 + 1] = verticesHolder[(i - longitude) * 8 + 1];
+                verticesHolder[i * 8 + 2] = verticesHolder[(i - longitude) * 8 + 2];
         	}
         	
         	// This condition forces north pole vertices to share the same coordinate.
         	else if (i <= longitude && i != 0)
         	{
-        		verticesHolder[i * 6 + 0] = verticesHolder[0 * 6 + 0];
-                verticesHolder[i * 6 + 1] = verticesHolder[0 * 6 + 1];
-                verticesHolder[i * 6 + 2] = verticesHolder[0 * 6 + 2];
+        		verticesHolder[i * 8 + 0] = verticesHolder[0 * 8 + 0];
+                verticesHolder[i * 8 + 1] = verticesHolder[0 * 8 + 1];
+                verticesHolder[i * 8 + 2] = verticesHolder[0 * 8 + 2];
         	}
         	
         	// This condition forces south pole vertices to share the same coordinate.
         	else if (i > (longitude * (latitude + 1)))
         	{
-        		verticesHolder[i * 6 + 0] = verticesHolder[(longitude * (latitude + 1)) * 6 + 0];
-                verticesHolder[i * 6 + 1] = verticesHolder[(longitude * (latitude + 1)) * 6 + 1];
-                verticesHolder[i * 6 + 2] = verticesHolder[(longitude * (latitude + 1)) * 6 + 2];
+        		verticesHolder[i * 8 + 0] = verticesHolder[(longitude * (latitude + 1)) * 8 + 0];
+                verticesHolder[i * 8 + 1] = verticesHolder[(longitude * (latitude + 1)) * 8 + 1];
+                verticesHolder[i * 8 + 2] = verticesHolder[(longitude * (latitude + 1)) * 8 + 2];
         	}
         	else
         	{
                 // Don't worry about the normal vectors yet... it gets pretty complicated 
                 // and it only affects lighting.
-        		double val = 0.01f * rand.nextDouble();
-                verticesHolder[i * 6 + 0] += val;
-                verticesHolder[i * 6 + 1] += val;
-                verticesHolder[i * 6 + 2] += val;	
+        		double val = 0.02f * rand.nextDouble();
+                verticesHolder[i * 8 + 0] += val;
+                verticesHolder[i * 8 + 1] += val;
+                verticesHolder[i * 8 + 2] += val;	
         	}
         	
         	// Vertex format goes like pppnnnpppnnn...
             // where p are position components, n are normal components.
-            float px = verticesHolder[i * 6 + 0];
-            float py = verticesHolder[i * 6 + 1];
-            float pz = verticesHolder[i * 6 + 2];
-            float nx = verticesHolder[i * 6 + 3];
-            float ny = verticesHolder[i * 6 + 4];
-            float nz = verticesHolder[i * 6 + 5];
+            float px = verticesHolder[i * 8 + 0];
+            float py = verticesHolder[i * 8 + 1];
+            float pz = verticesHolder[i * 8 + 2];
+            float nx = verticesHolder[i * 8 + 3];
+            float ny = verticesHolder[i * 8 + 4];
+            float nz = verticesHolder[i * 8 + 5];
+            float tx = verticesHolder[i * 8 + 6];
+            float ty = verticesHolder[i * 8 + 7];
             
             // This if-else passes the result of planetCenter.dst(x, y, z) to the elevationMap[].
             // I (Danny Ruan) will clean this up at some point because I don't like using empty thens
