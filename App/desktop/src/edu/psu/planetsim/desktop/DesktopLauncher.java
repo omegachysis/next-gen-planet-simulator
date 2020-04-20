35d8153764bb2e32d1264014aeef25b659505ffb
Matthew A. Robinson <omegachysis@outlook.com>
2020-04-20 16:57:07 -0400

    Enable multisampling anti-aliasing

--- App/desktop/src/edu/psu/planetsim/desktop/DesktopLauncher.java
+++ App/desktop/src/edu/psu/planetsim/desktop/DesktopLauncher.java
@@ -18,6 +18,8 @@ public class DesktopLauncher {
 		var config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		config.samples = 4; // MSAA
+		config.vSyncEnabled = true;
 		new LwjglApplication(new MainApp(), config);
 		//new LwjglApplication(new ThermalTest(), config);
 		//new LwjglApplication(new PlanetRenderTest(), config);
